b875a587a33b3f903157bc50f6b34cdbebe07b31
Gabriel MSSR <menezesgabrielmenezes@gmail.com>
2026-06-17 20:10:44 -0300

    fix: fix qdrant db
    Previously it was crashing due to having more than on client

--- src/indexer.py
+++ src/indexer.py
@@ -1,14 +1,14 @@
 import shutil
 from pathlib import Path
 
-# from langchain_text_splitters import Language
-# # Consider using this splitter by using the source code file extention to determine the language
 from langchain_ollama import OllamaEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
 
 from constants import CHUNK_OVERLAP, CHUNK_SIZE, EMBED_MODEL, INDEX_DIR
+from src.language import get_langchain_enum, get_language
 
 
 def indexer(code_path: Path):
@@ -62,7 +62,15 @@ def indexer(code_path: Path):
     #
 
     client = QdrantClient(path=str(INDEX_DIR))
-    QdrantVectorStore.from_texts(
-        client=client, texts=texts, embedding=embeddings, path=str(INDEX_DIR), collection_name="index"
-    )
-    client.close()
+    try:
+        # QdrantVectorStore.from_texts(client=client, texts=texts, embedding=embeddings, collection_name="index")
+        sample_vector = embeddings.embed_query("test")
+        vector_size = len(sample_vector)
+        client.create_collection(
+            collection_name="index",
+            vectors_config=VectorParams(size=vector_size, distance=Distance.COSINE),
+        )
+        db = QdrantVectorStore(client=client, collection_name="index", embedding=embeddings)
+        db.add_texts(texts=texts)
+    finally:
+        client.close()
